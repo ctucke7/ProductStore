361cef5f66a7f975265fab5b319fb02d32797f97
ctucke7 <ctucke7@gmu.edu>
2025-09-27 00:24:21 -0400

    Describe what you changed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,17 +1,15 @@
-import React from 'react';  
-import { Container, Flex, Text } from '@chakra-ui/react';
+import { Container, Flex, Text, Button, HStack, useColorMode } from '@chakra-ui/react';
 import { Link } from 'react-router-dom'; // since you’re using <Link to={"/"}>
-import { Button, HStack } from '@chakra-ui/react';
 import { PlusSquareIcon } from '@chakra-ui/icons';
-import { useColorMode } from '@chakra-ui/react';
 import { IoMoon } from 'react-icons/io5';
 import { LuSun } from 'react-icons/lu';
+import { useProductStore } from '../store/product';
 
 const Navbar = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <Container maxW="1140px" px={4}>
+    <Container maxW="1140px" px={4} >
       <Flex
         h={16}
         alignItems={'center'}
